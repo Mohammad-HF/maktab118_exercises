27d4eaa881bfe01c06056d64eabd410413aa46ea
hajifattah <mortymtfp@yahoo.com>
2024-09-26 17:37:16 +0330

    add drop event to FileInput component

--- Exercises/Week_13/1_HW_form_with_react_typescript/src/components/FileInput.tsx
+++ Exercises/Week_13/1_HW_form_with_react_typescript/src/components/FileInput.tsx
@@ -1,23 +1,30 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 export default function FileInput({collectValue}:{collectValue : CollectValue}) : JSX.Element {
     const [file,setFile] = useState<string>("");
     const [srcFile,setSrcFile] = useState<string>();
     const [borderC,setBorderC] = useState<string>("border-gray-400/70");
-    const onChangeHandler : React.ChangeEventHandler<HTMLInputElement> = (e)=>{
-     if(e.target.files?.[0] && e.target.files[0].size <= 10000000) {
-      setFile(e.target.files[0].name);
-      collectValue(e.target.files[0].name);
+    const onChangeHandler = (e : ChangeEvent<HTMLInputElement> | File) => {
+      const nowFile = "target" in e ? e.target.files?.[0] : e ;
+     if(nowFile && nowFile.size <= 10000000) {
+      setFile(nowFile.name);
+      collectValue(nowFile.name);
       setBorderC("border-green-400/70");
-     }else if(e.target.files?.[0] && e.target.files[0].size > 10000000){
+     }else if(nowFile && nowFile.size > 10000000){
       setFile("not valid");
       collectValue("");
       setBorderC("border-red-400/70")
      }
-     if (e.target.files?.[0] && e.target.files?.[0].type.includes("image")) {
-      setSrcFile(URL.createObjectURL(e.target.files[0]))
-     }else if(e.target.files?.[0] ) setSrcFile("./src/assets/file-check.svg")
+     if (nowFile && nowFile.type.includes("image")) {
+      setSrcFile(URL.createObjectURL(nowFile))
+     }else if(nowFile ) setSrcFile("./src/assets/file-check.svg")
      
+    }
+    const dropHandler : React.DragEventHandler<HTMLLabelElement> = (e)=>{
+      e.preventDefault();
+      console.log("dropped");
+      console.log(e.dataTransfer.files?.[0]);
+      onChangeHandler(e.dataTransfer.files?.[0])
     }
     const removeFile : React.MouseEventHandler<HTMLImageElement> = (e)=>{
       e.preventDefault();
@@ -31,6 +38,8 @@ export default function FileInput({collectValue}:{collectValue : CollectValue}) 
         <label
           htmlFor="fileI"
           className={`w-full text-sm ${file ? "text-black" : "text-grayApp"} text-center border-2 border-dashed ${borderC} px-8 py-8 inline-block`}
+          onDrop={dropHandler}
+          onDragOver={(e)=>{e.preventDefault()}}
         >
           <img
             className={`${srcFile ? "size-10" : "size-5"} inline-block mr-3 align-middle ${file ==="not valid" && "hidden"}`}
