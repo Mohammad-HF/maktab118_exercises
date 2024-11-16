c02c2b74d75d605099318941cb65ed0e78428da6
hajifattah <mortymtfp@yahoo.com>
2024-11-16 23:49:23 +0330

    add dashboard button to navbar 2-hide three button when user not login and 3- use token in home button instead of use in layout of tasks page for prevent delay of redirect to login page when user is not login

--- Exercises/Week_20/task_manager_with_nextjs/app/page.tsx
+++ Exercises/Week_20/task_manager_with_nextjs/app/page.tsx
@@ -1,7 +1,9 @@
+import { getToken } from "@/utils/session-managment";
 import Link from "next/link";
 import { FaExternalLinkAlt } from "react-icons/fa";
 
-export default function Home() {
+export default async function Home() {
+      const userToken = await getToken();
   return (
     <div className=" bg-[#7748cc] min-h-[calc(100vh-128px)]">
       <img className="sm:h-1/2 max-h-[560px] min-h-[300px] w-full sm:max-w-screen-xl" src="To-do_List_Template.png" alt="" />
@@ -11,7 +13,7 @@ export default function Home() {
           prioritizing, and optimizing tasks using our To-Do List Template
         </p>
         <Link
-          href={"tasks"}
+          href={`${userToken ? "tasks" : "/login"}`}
           className="flex items-center bg-[#fcba64] hover:bg-[#cf9549] rounded-2xl w-fit px-3 mt-10"
         >
           <button className="text-lg text-slate-900 py-1 font-semibold ">
--- Exercises/Week_20/task_manager_with_nextjs/components/navbar/Menu.tsx
+++ Exercises/Week_20/task_manager_with_nextjs/components/navbar/Menu.tsx
@@ -20,14 +20,22 @@ export const Menu: React.FC<{
       </button>
 
       {show && (
-        <div className="bg-slate-900 flex flex-col w-52 absolute top-10 right-0 p-2 gap-y-2 text-sm items-center rounded-md">
-          <div className="flex gap-x-2 ">
+        <div className="bg-slate-900 flex flex-col w-80 absolute top-10 -right-2 p-2 gap-y-2 text-sm items-center rounded-md">
+          <div className={`flex gap-x-2 ${userToken ? "flex" : "hidden"}`}>
             <Link href={"/tasks/in-progress"}>
               <button
                 onClick={() => setShow((prev) => !prev)}
                 className="text-white font-semibold border rounded-md py-1 px-2 bg-[#f7be82] hover:bg-[#ca945e]"
               >
                 In Progress
+              </button>
+            </Link>
+            <Link href={"/tasks"}>
+              <button
+                onClick={() => setShow((prev) => !prev)}
+                className="text-white font-semibold border rounded-md py-1 px-2 bg-slate-600 hover:bg-slate-700"
+              >
+                Dashboard
               </button>
             </Link>
             <Link href={"/tasks/completed"}>
